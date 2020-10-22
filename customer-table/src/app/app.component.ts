68d34d39758c0aeeec245ce827ba5c2d6ddb6b2f
Brennon Nuckols <brennuck@gmail.com>
2020-10-22 16:03:23 -0600

    created customers component

--- customer-table/src/app/app.component.ts
+++ customer-table/src/app/app.component.ts
@@ -3,11 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({ 
   selector: 'app-root',
   template: `
-    <h1>Hello World</h1>
+    <app-customers> </app-customers>
   `
 })
 export class AppComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
--- customer-table/src/app/app.module.ts
+++ customer-table/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { CustomersComponent } from './customers/customers.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, CustomersComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
