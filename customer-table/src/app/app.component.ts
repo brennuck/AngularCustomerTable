4b36cafc8a1504feda3cf6aa1604a0ed51e5b66c
Brennon Nuckols <brennuck@gmail.com>
2020-10-26 14:44:52 -0600

    Update. Added table and filterbox

--- customer-table/src/app/app.component.ts
+++ customer-table/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({ 
   selector: 'app-root',
   template: `
-    <app-customers> </app-customers>
+    <app-customers></app-customers>
   `
 })
 export class AppComponent implements OnInit {
--- customer-table/src/app/app.module.ts
+++ customer-table/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CustomersModule } from './customers/customers.module';
 import { AppComponent }  from './app.component';
-import { CustomersComponent } from './customers/customers.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent, CustomersComponent ],
+  imports:      [ BrowserModule, CustomersModule ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
