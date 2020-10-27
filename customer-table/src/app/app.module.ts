4e9e5d93fe642625ee4eb9afd1765c7e843894c1
Brennon Nuckols <brennuck@gmail.com>
2020-10-27 15:20:54 -0600

    added app routing module

--- customer-table/src/app/app.module.ts
+++ customer-table/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { CoreModule } from './core/core.module';
 import { CustomersModule } from './customers/customers.module';
 import { AppComponent }  from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, CoreModule, CustomersModule, SharedModule ],
+  imports:      [ BrowserModule, CoreModule, CustomersModule, SharedModule, AppRoutingModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
