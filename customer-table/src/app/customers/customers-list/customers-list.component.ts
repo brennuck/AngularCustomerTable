1665cca627129147525dbe15880d1c7596b0f778
Brennon Nuckols <brennuck@gmail.com>
2020-10-27 15:11:29 -0600

    sorting works

--- customer-table/src/app/customers/customers-list/customers-list.component.ts
+++ customer-table/src/app/customers/customers-list/customers-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { SorterService } from 'src/app/core/sorter.service';
 
 import { ICustomer } from 'src/app/shared/interfaces';
 
@@ -23,7 +24,7 @@ export class CustomersListComponent implements OnInit {
     customersOrderTotal: number;
     currencyCode: string = 'USD';
     
-    constructor() {}
+    constructor( private sorterService: SorterService) {}
     
     ngOnInit() {
         
@@ -52,6 +53,6 @@ export class CustomersListComponent implements OnInit {
     }
 
     sort(prop: string) {
-        // A sorter service will handle the sorting
+        this.sorterService.sort(this.filteredCustomers, prop)
     }
 }
